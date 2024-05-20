bcd233af0f6944ddb2693cf571c5a995fe64000c
Shengjie Xu <shengjiex98@gmail.com>
2024-05-19 23:32:27 -0400

    Add package code and simple test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using ControlBenchmarks
 using Test
 
 @testset "ControlBenchmarks.jl" begin
-    # Write your tests here.
+    sys = benchmarks[:F1T]
+    K = delay_lqr(sys, 0.02)
+
+    @test K ≈ [0.58 0.93 0.35] atol=0.01
 end
